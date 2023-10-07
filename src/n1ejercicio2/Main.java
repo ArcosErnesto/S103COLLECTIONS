e80243a577f27b27afa67e4cd49e6d00220db9a2
arcosernesto@gmail.com <arkulinius@gmail.com>
2023-10-07 08:53:41 +0200

    Acabado n1 ex2

---
+++ src/n1ejercicio2/Main.java
@@ -0,0 +1,42 @@
+package n1ejercicio2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+
+		System.out.println("***Print list***");
+
+		for (Integer integer : list) {
+			System.out.println(integer);
+		}
+
+		List<Integer> reverseList = new ArrayList<Integer>();
+
+		ListIterator<Integer> it = list.listIterator(list.size());
+
+		while (it.hasPrevious()) {
+			Integer num = it.previous();
+
+			reverseList.add(num);
+		}
+
+		System.out.println("***Print reverse list***");
+
+		for (Integer integer : reverseList) {
+			System.out.println(integer);
+		}
+
+	}
+
+}
